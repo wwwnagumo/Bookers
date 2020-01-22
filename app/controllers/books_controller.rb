0f5bb01986f9873ad566a38bf5e9c3cb4350ab17
wwwnagumo <www.03140624@outlook.jp>
2020-01-22 15:44:54 +0900

    [Add]4-8バリデーションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
 
   def create
   	@book = Book.new(book_params)
+    @books = Book.all
   	if @book.save
   	  redirect_to book_path(@book),notice: "Book was successfully created."
     else
